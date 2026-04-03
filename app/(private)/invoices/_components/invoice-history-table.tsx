e55f25e6ef190d790da1a8785efff392d215d4f1
Tophy Lad <tophyrakotomavo@gmail.com>
2026-04-03 11:48:13 +0300

    feat: add invoice history table and improve status tag rendering and UI styling

--- app/(private)/invoices/_components/invoice-history-table.tsx
+++ app/(private)/invoices/_components/invoice-history-table.tsx
@@ -29,9 +29,8 @@ export const InvoiceHistoryTable = ({
   const router = useRouter();
 
   const getDetailsUrl = (record: InvoiceHistoryItem) => {
-    return `/invoices?periodId=${format(parseISO(record.periodStart), "yyyy-MM")}${
-      isOwner ? `&memberId=${record.userId}` : ""
-    }`;
+    return `/invoices?periodId=${format(parseISO(record.periodStart), "yyyy-MM")}${isOwner ? `&memberId=${record.userId}` : ""
+      }`;
   };
 
   const columns = [
