320a63046754ac377f0ee45d5a90a350adac8d9e
Tophy Lad <tophyrakotomavo@gmail.com>
2026-08-14 15:04:12 +0300

    Remove organization_finance_emails table and related constraints from migration 0013

--- db/migration/0013_sad_the_twelve.sql
+++ db/migration/0013_sad_the_twelve.sql
@@ -9,14 +9,6 @@ CREATE TABLE "organization_attendance_settings" (
 	"updated_at" timestamp DEFAULT now() NOT NULL
 );
 --> statement-breakpoint
-CREATE TABLE "organization_finance_emails" (
-	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
-	"organization_id" uuid NOT NULL,
-	"email" text NOT NULL,
-	"created_at" timestamp DEFAULT now()
-);
---> statement-breakpoint
 ALTER TABLE "organization_attendance_settings" ADD CONSTRAINT "organization_attendance_settings_organization_id_organizations_id_fk" FOREIGN KEY ("organization_id") REFERENCES "public"."organizations"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "organization_finance_emails" ADD CONSTRAINT "organization_finance_emails_organization_id_organizations_id_fk" FOREIGN KEY ("organization_id") REFERENCES "public"."organizations"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 CREATE UNIQUE INDEX "organization_attendance_type_unique" ON "organization_attendance_settings" USING btree ("organization_id","type");--> statement-breakpoint
 CREATE UNIQUE INDEX "organization_finance_email_unique" ON "organization_finance_emails" USING btree ("organization_id","email");
